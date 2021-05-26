1e53e948ca4eea0fd331893cbdbc02ac0fbd9e7d
Jean-Luc <jean-luc.a@ideafactory.fr>
2021-05-26 12:22:20 +0200

    Add home component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'address-book';
+export class AppComponent implements OnInit {
+  private _title = 'address-book';
+
+  ngOnInit(): void {
+
+  }
+
+  public get title(): string {
+    return this._title;
+  }
+
+  public set title(title: string) {
+    this._title = title;
+  }
 }
